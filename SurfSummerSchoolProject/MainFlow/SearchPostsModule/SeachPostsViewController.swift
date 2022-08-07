52b09b58f448f196e3f9b16374d55e161c8b7313
GolubeykovAnton <ds6580@yandex.ru>
2022-08-07 15:27:01 +0300

    Добавлена кнопка перехода на searchScreen

---
+++ SurfSummerSchoolProject/MainFlow/SearchPostsModule/SeachPostsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SeachPostsViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Антон Голубейков on 07.08.2022.
+//
+
+import UIKit
+
+class SeachPostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
